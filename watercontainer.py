fa25f5f6c1d7ef83d8a3936b266a29683e684c12
ShajidShahriar <shajidsr12@gmail.com>
2025-02-18 23:18:22 +0600

    added another solution

--- watercontainer.py
+++ watercontainer.py
@@ -17,9 +17,29 @@ def maxArea(heights):
         waterVols.append((right - left) * min_height)
         if min_height == heights[left]:
             left+=1
-        elif min_height == heights[right]:
+        else:
             right-=1
     return max(waterVols)
 height = [1,7,2,5,4,7,3,6]
 
 print(maxArea(height))
+
+
+#another neat technique
+# def maxArea(heights):
+#     left, right = 0, len(heights) - 1
+#     max_water = 0  # Track the maximum volume
+#
+#     while left < right:
+#         min_height = min(heights[left], heights[right])
+#         max_water = max(max_water, (right - left) * min_height)
+#
+#         if heights[left] < heights[right]:
+#             left += 1
+#         else:
+#             right -= 1
+#
+#     return max_water
+#
+# height = [1,7,2,5,4,7,3,6]
+# print(maxArea(height))
